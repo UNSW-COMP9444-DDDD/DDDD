f487b486c3716e28fe3cf65d4704fcc9cd20952a
Kai Huang <z5335227@ad.unsw.edu.au>
2023-07-10 13:36:38 +1000

    create ui

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # DDDD
+
+乳腺癌检测项目
+
+## 依赖
+
+- Python版本：3.7+
+- Streamlit版本：0.88.0
+- 上传项目的时候记得添加gitignore文件（稍后我写一下）
+
+## 安装
+
+1. 克隆项目到本地：git clone https://github.com/your_username/your_project.git
+2. 进入项目目录：cd DDDD
+3. 安装所需依赖：pip install -r requirements.txt (稍后上传)
+## 启动项目
+
+在ui目录下执行以下命令启动Streamlit应用程序：
+
+```shell
+streamlit run app.py
+
+
+
+
---
+++ ui/front.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import torch
+from torchvision.transforms import ToTensor
+from PIL import Image
+
+# 假设模型返回10个类别的预测结果
+num_classes = 10
+
+# 创建随机初始化的模型
+model = torch.nn.Sequential(
+    torch.nn.Linear(3 * 224 * 224, 512),
+    torch.nn.ReLU(),
+    torch.nn.Linear(512, num_classes),
+)
+
+# 加载示例图片用于演示
+sample_image = Image.open("sample_image.jpg")
+
+st.title("Breast Cancer detection")
+st.write("upload a image")
+
+# 创建上传文件界面
+uploaded_file = st.file_uploader("upload", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+
+    # 将图像转换为张量，并进行模型预测
+    image_tensor = ToTensor()(image).unsqueeze(0)
+    flattened_image = image_tensor.view(-1, 3 * 224 * 224)
+    prediction = model(flattened_image)
+    _, predicted_class = torch.max(prediction, 1)
+
+    st.write("预测类别：", predicted_class.item())
+
+st.write("Images：")
+st.image(sample_image, caption='Breast Cancer detection Image', use_column_width=True)
